908cf0dee446b5ea610a391f001bf9f5c9baa787
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-10-31 18:32:05 +0100

    nix-bitcoin-release 0.0.56

--- nix-bitcoin-release.nix
+++ nix-bitcoin-release.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.55.tar.gz";
-  sha256 = "sha256-ctlB+vOPEnDV5jPznEoQU6ES1MLcM1VpC3IOcAOtJSA=";
+  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.56.tar.gz";
+  sha256 = "sha256-SsWhpGpJOh6xM+3y+QJ921kcQPjPzpytcOQegwy2pi0=";
 }
