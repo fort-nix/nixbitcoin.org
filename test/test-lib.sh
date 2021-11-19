bbcd704f8efff330a908ca66a1d80fa70fd3ed6d
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-11-19 02:13:49 +0100

    add test

---
+++ test/test-lib.sh
@@ -0,0 +1,96 @@
+burstLimitIgnoreInternalResponse=
+
+getBurstLimit() {
+  type=$1
+  url=$2
+  max=100
+
+  if [[ $type == GET ]]; then
+    okStatus=200
+    curlArgs=()
+  else
+    # POST
+    okStatus=302
+    curlArgs=(-X POST -d '')
+  fi
+
+  for ((i=0; i<$max; i++)); do
+    status=$(getStatusCode "${curlArgs[@]}" "$url")
+    if [[ $status != $okStatus ]]; then
+      if [[ $status == 429 ]]; then
+        echo $i
+        return
+      elif [[ ! $burstLimitIgnoreInternalResponse ]]; then
+        >&2 echo "Unexpected status code ($status) for $url"
+        return 1
+      fi
+    fi
+  done
+  >&2 echo "Max burst limit ($max) exceeded for $url"
+  return 1
+}
+
+getStatusCode() {
+  maxTime=
+  if [[ $burstLimitIgnoreInternalResponse ]]; then
+    maxTime="--max-time 0.02"
+  fi
+  curl -s $maxTime -o /dev/null -w "%{http_code}" "$@"
+}
+
+assertBurstLimit() {
+  expected=$1
+  type=$2
+  url=$3
+  actual=$(getBurstLimit "$type" "$url")
+  tolerance=8
+  if ((actual < expected || actual - expected > tolerance)); then
+    >&2 echo "Unexpected burst limit: $actual. Expected: $expected ($type $url)"
+    return 1
+  fi
+}
+
+assertMatches() {
+  expected="$1"
+  actual="$2"
+  if [[ $actual != $expected ]]; then
+    echo
+    echo 'Pattern does not match'
+    echo 'Expected:'
+    echo "$expected"
+    echo
+    echo 'Actual:'
+    echo "$actual"
+    echo
+    return 1
+  fi
+}
+
+waitForPort() {
+  address=$1
+  port=$2
+  attempts=200
+  while ! { exec 3>/dev/tcp/$address/$port && exec 3>&-; } &>/dev/null; do
+    ((attempts-- == 0)) && { echo "Error: $address:$port is unreachable"; exit 1; }
+    sleep 0.01
+  done
+}
+
+restartNginx() {
+  # Run `reset-failed` to reset service restart counters.
+  # This avoids error `start-limit-hit` after many restarts
+  c bash -c 'systemctl restart nginx && systemctl reset-failed nginx'
+  waitForPort $ip 80
+}
+
+WANStatus=
+isWANenabled() {
+  if [[ ! $WANStatus ]]; then
+    if c bash -c '[[ $WANEnabled ]]'; then
+      WANStatus=1
+    else
+      WANStatus=0
+    fi
+  fi
+  [[ $WANStatus == 1 ]]
+}
---
+++ test/test.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This script is run in a container shell by
+# ./cmds/run-test
+
+# To manually run (parts of) the test, start the nix shell of this project, run
+# `container website` or `co website` and execute test commands in the container shell.
+
+cd "${BASH_SOURCE[0]%/*}"
+source ./test-lib.sh
+
+trap 'echo Error at line $LINENO' ERR
+
+echo -n "Wait until btcpayserver serves content..."
+while [[ $(getStatusCode http://$ip/donate) != 200 ]]; do
+  sleep 0.1
+done
+echo "Done"
+
+echo "Test rate limits"
+assertBurstLimit 20 GET http://$ip
+restartNginx # Reset rate limit counters
+assertBurstLimit 20 GET http://$ip/donate
+restartNginx
+
+if isWANenabled; then
+  echo "Running tests in online mode"
+else
+  # For the following invoice-generating requests, btcpayserver
+  # requires an internet connection for rate-fetching (even for the
+  # Bitcoin-only LNURL invoice).
+  # Set this variable to ignore the btcpayserver failure and just check for the
+  # nginx-generated status code (429) that indicates active rate limiting.
+  burstLimitIgnoreInternalResponse=1
+fi
+
+assertBurstLimit 5 POST http://$ip/donate
+restartNginx
+
+echo "Success"
