0cd719ee7ee400869fffac577f03f6498b11026c
nixbitcoin <nixbitcoin@i2pmail.org>
2022-02-13 13:45:42 +0100

    nix-bitcoin-release 0.0.66

--- nix-bitcoin-release.nix
+++ nix-bitcoin-release.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.64.tar.gz";
-  sha256 = "sha256-5Qb6ifgIXHD/FXwpGGXGjOhTUe9tqFmypufTRJA58Bo=";
+  url = "https://github.com/fort-nix/nix-bitcoin/archive/v0.0.66.tar.gz";
+  sha256 = "sha256-IRPGHdXVzDYGP3YQcx5S7Een0cd/zvRpj/m+oTMoLHc=";
 }
