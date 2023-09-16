250ee488bfc380b9b5227613ea0ec54857183171
Erik Arvstedt <erik.arvstedt@gmail.com>
2023-09-16 17:35:36 +0200

    update nix-bitcoin

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nix-bitcoin = {
-    url = "github:erikarvstedt/nix-bitcoin/mempool-ea-clboss";
+    url = "github:erikarvstedt/nix-bitcoin/mempool-ea";
     inputs.extra-container.follows = "extra-container";
   };
   inputs.nixos-mailserver = {
