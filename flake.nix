602cad933814701ef1f607a542960231163e2ca9
Erik Arvstedt <erik.arvstedt@gmail.com>
2025-06-11 17:48:17 +0200

    update nix-bitcoin

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nix-bitcoin = {
-    url = "github:erikarvstedt/nix-bitcoin?ref=nixos-25.05";
+    url = "github:fort-nix/nix-bitcoin";
     inputs.extra-container.follows = "extra-container";
   };
   inputs.nixos-mailserver = {
