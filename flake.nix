c2fc0769b19d62c3d3584811d08a6c72c0acf524
Erik Arvstedt <erik.arvstedt@gmail.com>
2025-09-20 08:26:10 +0200

    update nix-bitcoin

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nix-bitcoin = {
-    url = "github:erikarvstedt/nix-bitcoin?ref=joinmarket-directory-nodes";
+    url = "github:fort-nix/nix-bitcoin";
     inputs.extra-container.follows = "extra-container";
   };
   inputs.nixos-mailserver = {
